c63713be14382be141f1d6651567bdf41e5dcd1e
Arafat <arafatinbusiness@gmail.com>
2026-05-23 14:43:29 +0600

    m

--- components/rich-text-editor.tsx
+++ components/rich-text-editor.tsx
@@ -124,6 +124,7 @@ export function RichTextEditor({
 
   const editor = useEditor({
     immediatelyRender: false,
+    autofocus: false,
     extensions: [
       StarterKit.configure({
         heading: {
